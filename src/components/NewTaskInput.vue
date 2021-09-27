26f3bfb4cbf7be3e3e3f5dcd110d82b972f7e90b
NabeelKM <nabeelkahlil403@gmail.com>
2021-09-27 20:16:36 +0700

    adde store and add functionality

--- src/components/NewTaskInput.vue
+++ src/components/NewTaskInput.vue
@@ -3,13 +3,27 @@
     <input
       class="task-content input-field center-input"
       placeholder="What do I do today?"
+      v-model="inputField"
+      @keyup.enter="handleNewTodo"
     >
   </div>
 </template>
 
 <script>
-export default {
+import { ref } from 'vue';
+import { TodoList } from '../utils/store';
 
+export default {
+  setup() {
+    const inputField = ref('')
+    return {
+      inputField,
+      handleNewTodo: () => {
+        TodoList.createTodo(inputField.value);
+        inputField.value = ''
+      }
+    }
+  }
 }
 </script>
 
